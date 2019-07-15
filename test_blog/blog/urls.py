e58c04807c7a3aead5fdf12fa83c64cd94a7d131
Fasther <30392053+Fasther@users.noreply.github.com>
2019-07-15 21:20:11 +0200

    HTML templates 172. lesson

--- test_blog/blog/urls.py
+++ test_blog/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from blog import views
 
+app_name = "blog"
+
 urlpatterns = [
     path("about/", views.AboutView.as_view(), name="about"),
     path("", views.PostListView.as_view(), name="post_list"),
